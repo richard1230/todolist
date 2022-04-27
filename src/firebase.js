d5168018d304521e35d778ab1804c397fc570193
richard Hu <yongjinhu1230@gmail.com>
2022-04-27 17:24:39 +0800

    AddFirebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyDZSkZwCZOvcAyZqq1Jc7te6vQlBMwXMQM",
+  authDomain: "todolist-a5b26.firebaseapp.com",
+  databaseURL: "https://todolist-a5b26-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "todolist-a5b26",
+  storageBucket: "todolist-a5b26.appspot.com",
+  messagingSenderId: "954836380739",
+  appId: "1:954836380739:web:320215f65e3e90edc9f8f6"
+});
+
+export { firebaseConfig as firebase };

67a2b9ed02b29211bfed9096460343f8745ec42d
richard Hu <yongjinhu1230@gmail.com>
2022-05-02 21:20:03 +0800

    Back_to_adding_more_to_our_React_hooks_useTasks_useProjects

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -6,6 +6,8 @@ import {collatedTasksExist} from "../helpers";
 
 export const useTasks = selectProject => {
   const [tasks, setTasks] = useState([]);
+  const [archivedTasks, setArchivedTasks] = useState([]);
+
 
   useEffect(() => {
     let unsubscribe = firebase
@@ -20,8 +22,53 @@ export const useTasks = selectProject => {
           ? (unsubscribe = unsubscribe.where('date', '==', moment().format('DD/MM/YYYY')))
           : selectProject === 'INBOX' || selectProject === 0
             ? (unsubscribe = unsubscribe.where('date', '==', ''))
-            : unsubscribe
+            : unsubscribe;
+    unsubscribe = unsubscribe.onSnapshot(snapshot => {
+      const newTasks = snapshot.docs.map(task => ({
+        id: task.id,
+        ...task.data(),
+      }));
 
-  }, [selectProject])
+      setTasks(
+        selectProject === 'NEXT_7'
+          ? newTasks.filter(task =>
+            moment(task.data, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+            task.archived !== true)
+          :
+          newTasks.filter(task => task.archived !== true)
+      );
+      setArchivedTasks(newTasks.filter(task => task.archived !== false));
+    });
 
+    return () => unsubscribe();
+
+  }, [selectProject]);
+
+  return {tasks, archivedTasks};
 }
+
+export const useProjects = ()=>{
+  const [projects,setProjects] = useState();
+  useEffect(()=>{
+    firebase.firestore()
+      .collection('projects')
+      .where('userId','==','0101010101')
+      .orderBy('projectId')
+      .get()
+      .then(snapshot =>{
+        const allProjects = snapshot.docs.map(
+          project =>(
+            {
+              ...project.data(),
+              docId: project.id,}));
+          if(JSON.stringify(allProjects) !== JSON.stringify(projects)){
+            setProjects(allProjects);
+          }
+      });
+    },
+    [projects]);
+  return {projects,setProjects}
+}
+
+
+

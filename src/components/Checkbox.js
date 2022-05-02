812bb4fd20d8c547a67ee43956698ea5cf8bfd52
richard Hu <yongjinhu1230@gmail.com>
2022-05-02 22:45:15 +0800

    Checkbox_component

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,25 +1,25 @@
 import {firebase} from "../firebase";
 import React from "react";
 
-export const Checkbox =({id})=>{
- const archiveTask = () => {
-   firebase.firestore()
-     .collection('tasks')
-     .doc(id)
-     .update(
-       {
-         archived:true,
-       }
-     );
- };
+export const Checkbox = ({id}) => {
+  const archiveTask = () => {
+    firebase.firestore()
+      .collection('tasks')
+      .doc(id)
+      .update(
+        {
+          archived: true,
+        }
+      );
+  };
 
- return (
-   <div
-    className="checkbox-holder"
-    data-testid = "checkbox-action"
-    onClick={()=>archiveTask()}>
-     <span  className="checkbox"/>
-   </div>
- )
+  return (
+    <div
+      className="checkbox-holder"
+      data-testid="checkbox-action"
+      onClick={() => archiveTask()}>
+      <span className="checkbox"/>
+    </div>
+  )
 
 };
